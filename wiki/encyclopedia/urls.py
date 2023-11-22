033071ec378948ba4f4723c25bc35b530c9410f4
shiyu <shiyu_l7@163.com>
2023-11-22 15:57:17 +0800

    search

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:name>", views.title, name="title"),
+    path("search", views.search, name="search"),
+    path("random", views.random, name="random"),
+    path("new", views.new, name="new"),
+    path("wiki/<str:name>/edit", views.edit, name="edit"),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -32,3 +32,76 @@ def title(request, name):
     return render(request, "encyclopedia/title.html", {
         "entry": entry
     })
+
+
+def search(request):
+    query = request.GET['q']
+    if not query:
+        return redirect(index)
+
+    cap_query = util.get_capitalized_title(query)
+    if cap_query:
+        return redirect(title, name=cap_query)
+
+    return render(request, "encyclopedia/search.html", {
+        "matches": [name for name in util.list_entries() if query.lower() in name.lower()],
+        "query": query
+    })
+
+
+def random(request):
+    return redirect(title, name=random_choice(util.list_entries()))
+
+
+def new(request):
+    if request.method == 'POST':
+        name = request.POST.get('title')
+        body = request.POST.get('body')
+
+        if not name:
+            error_message = "Title cannot be empty."
+            return render(request, "encyclopedia/new.html", {
+                "error_message": error_message,
+                "title": name,
+                "body": body
+            })
+
+        if util.get_entry(name):
+            error_message = "An entry with this title already exists. Choose a different title."
+            return render(request, "encyclopedia/new.html", {
+                "error_message": error_message,
+                "title": name,
+                "body": body
+            })
+
+        util.save_entry(name, body)
+        return redirect('title', name)
+
+    return render(request, "encyclopedia/new.html")
+
+
+def edit(request, name):
+    cap_name = util.get_capitalized_title(name)
+
+    if not cap_name:
+        raise Http404("404 not found")
+
+    if name != cap_name:
+        return redirect(edit, cap_name)
+
+    if request.method == 'POST':
+        body = request.POST.get('body')
+
+        # Save the edited entry
+        util.save_entry(name, body)
+
+        # Redirect back to the edited entry
+        return redirect('title', name=name)
+
+    # GET request
+    markdown = util.get_entry(name)
+
+    return render(request, "encyclopedia/edit.html", {
+        "title": name,
+        "body": markdown
+    })
